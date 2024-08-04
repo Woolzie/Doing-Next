f2f143e4f9a931d9d41268faf70dfe3701841e04
Woolzie <snana.swag@gmail.com>
2024-08-04 14:58:15 +0530

    clean: fixed ui and added a button

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
-import './globals.css'
+import Button from './components/Button'
+
 const page = () => {
   return (
     <>
-<h1 class="text-3xl font-bold underline">Sana</h1>
-<p class="bg-purple-500 text-white text-center p-8 rounded-lg shadow-lg"> Trying to distract myself from existential crisis with some linussy OwO </p>
-<p>Professional in breathing, i would say i do it everyday on a consistent basis</p>
-<p>Sometimes i wish i didnt TWT </p>
+    <div className='flex-col items-center h-screen p-10 '>
+<h1 className='font-bold font-italic text-center flex flex-col text-brown-300 text-4xl p-18'>Sana</h1>
+<p className="mt-6 bg-blue-300 p-5 text-yellow-200  text-center  rounded-md shadow-lg flex flex-grow items-center justify-center text-l"> Trying to distract myself from existential crisis with some linussy OwO <br></br> 
+Professional in breathing, i would say i do it everyday on a consistent basis<br></br>
+Sometimes i wish i didnt TWT </p>
+<Button></Button>
+</div>
+
   </>)
 }
 
---
+++ src/app/pages/Home/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const Home = () => {
+  return (
+    <>
+    <div className='justify-center flex items-center h-screen'>  
+       <h1 className="text-5xl font-italic text-center text-yellow-600 bg-blue-200 p-4 hover:bg-blue-300 shadown-lg rounded-lg">Home
+      <p className="mt-6 text-lg text-left text-gray-700 bg-green-100 p-4 rounded-md shadow-lg hover:text-green-800">
+        Welcome to the homepage of our e-commerce website.
+      </p>
+      </h1>
+      
+    </div>
+    </>  
+)
+}
+
+export default Home 
